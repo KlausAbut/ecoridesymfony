4379cab5d1ac986700de8029bd91582c9208580a
Claudiu <claudiu.abutnariti@gmail.com>
2025-05-26 09:37:22 +0200

    heroku

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function contact(Request $request): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
         if ($request->isMethod('POST')) {
-            $this->addFlash('success', '✅ Votre message a bien été envoyé !');
+            $nom = $request->request->get('nom');
+            $email = $request->request->get('email');
+            $message = $request->request->get('message');
+
+            $emailMessage = (new Email())
+                ->from($email)
+                ->to('contact@ecoride.fr') // destinataire fictif
+                ->subject("Nouveau message de $nom")
+                ->text("Email : $email\n\nMessage :\n$message");
+
+            $mailer->send($emailMessage);
+
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
             return $this->redirectToRoute('contact');
         }
 

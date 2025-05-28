a83dbe89da6c3932691d5cdee2580e49bffec07c
Claudiu <claudiu.abutnariti@gmail.com>
2025-05-29 01:07:37 +0200

    Fix migration is_conducteur pour Postgres

--- migrations/Version20250410093803.php
+++ migrations/Version20250410093803.php
@@ -21,7 +21,7 @@ final class Version20250410093803 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE user ADD is_conducteur TINYINT(1) NOT NULL
+            ALTER TABLE "user" ADD COLUMN is_conducteur BOOLEAN NOT NULL
         SQL);
     }
 
@@ -29,7 +29,7 @@ final class Version20250410093803 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE user DROP is_conducteur
+            ALTER TABLE "user" DROP COLUMN is_conducteur
         SQL);
     }
 }
